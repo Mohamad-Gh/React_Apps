8500f4830564c5820940c0c8930d44d467a5f4be
unknown <mohamad.ghalebizade@gmail.com>
2024-04-24 12:45:01 -0700

    Add Footer

--- car-rental-app/src/components/App.jsx
+++ car-rental-app/src/components/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import Header from "./Header";
-
+import Footer from "./Footer";
 function App() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
