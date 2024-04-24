c1f203cb571a34fde2089709532aefd3584bfa83
unknown <mohamad.ghalebizade@gmail.com>
2024-04-24 12:42:04 -0700

    Init Car Rental App

---
+++ car-rental-app/src/components/Header.jsx
@@ -0,0 +1,59 @@
+import logo from "../Assets/images/logo.png";
+
+function Header() {
+  return (
+    <header>
+      <div class="full-container">
+        <div class="container-header">
+          <img class="company_logo" src={logo} alt="Company Logo" />
+          <nav>
+            <ul class="navigator">
+              <li>
+                <a href="">Home</a>
+              </li>
+              <li>
+                <a href="">Vehicle Model</a>
+              </li>
+              <li>
+                <a href="">Testimonials</a>
+              </li>
+              <li>
+                <a href="">Our Team</a>
+              </li>
+              <li>
+                <a href="./public/About/about.html">About</a>
+              </li>
+            </ul>
+          </nav>
+          <div class="sign-in">
+            <div>
+              <button class="btn">Sign in</button>
+              <button class="btn btn-nav">Register</button>
+            </div>
+          </div>
+          <div class="mobile-hamb">
+            <svg
+              id="menu-bar"
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="tabler-icon tabler-icon-menu-2"
+            >
+              <path d="M4 6l16 0"></path>
+              <path d="M4 12l16 0"></path>
+              <path d="M4 18l16 0"></path>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
