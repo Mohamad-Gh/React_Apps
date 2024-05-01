5e75fe8efd4ba58820bf2869a81fb855b1b4b566
unknown <mohamad.ghalebizade@gmail.com>
2024-04-30 22:56:10 -0700

    Create Faq component

--- car-rental-app/src/components/Main.jsx
+++ car-rental-app/src/components/Main.jsx
@@ -8,6 +8,7 @@ import BlackDivider from "./BlackDivider";
 import HeroSection from "./HeroSection";
 import ContentCard from "./ContentCard";
 import Booking from "./Booking";
+import Faq from "./Faq";
 
 function Main() {
   return (
@@ -82,64 +83,7 @@ function Main() {
         span="24/7"
         text2="Support."
       />
-      <section className="faq">
-        <div className="container-faq">
-          <div className="faq-title">
-            <h3>FAQ</h3>
-            <h2>Frequently Asked Questions</h2>
-            <p>
-              Frequently Asked Questions About the Car Rental Booking Process on
-              Our Website: Answers to Common Concerns and Inquiries.
-            </p>
-          </div>
-          <div className="faq-qeustions-container">
-            <div className="QA-box">
-              <div className="questions selected">
-                <div>What is special about comparing rental car deals?</div>
-                <i className="fa-solid fa-chevron-down"></i>
-              </div>
-              <p className="responses selected">
-                Comparing rental car deals is important as it helps find the
-                best deal that fits your budget and requirements, ensuring you
-                get the most value for your money. By comparing various options,
-                you can find deals that offer lower prices, additional services,
-                or better car models. You can find car rental deals by
-                researching online and comparing prices from different rental
-                companies.
-              </p>
-            </div>
-            <div className="QA-box">
-              <div className="questions">
-                <div>How do I find the car rental deals?</div>
-                <i className="fa-solid fa-chevron-down"></i>
-              </div>
-              <p className="responses">
-                You can find car rental deals by researching online and
-                comparing prices from different rental companies. Websites such
-                as Expedia, Kayak, and Travelocity allow you to compare prices
-                and view available rental options. It is also recommended to
-                sign up for email newsletters and follow rental car companies on
-                social media to be informed of any special deals or promotions.
-              </p>
-            </div>
-            <div className="QA-box">
-              <div className="questions">
-                <div>How do I find such low rental car prices?</div>
-                <i className="fa-solid fa-chevron-down"></i>
-              </div>
-              <p className="responses">
-                Book in advance: Booking your rental car ahead of time can often
-                result in lower prices. Compare prices from multiple companies:
-                Use websites like Kayak, Expedia, or Travelocity to compare
-                prices from multiple rental car companies. Look for discount
-                codes and coupons: Search for discount codes and coupons that
-                you can use to lower the rental price. Renting from an
-                off-airport location can sometimes result in lower prices.
-              </p>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Faq />
       <section className="app">
         <div className="app-container">
           <h2>Download our app to get most out of it</h2>
