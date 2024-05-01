77324d9e49fba7b7452fedcfabb2dcba6507df09
unknown <mohamad.ghalebizade@gmail.com>
2024-04-30 22:16:52 -0700

    Make clickhandeler shorter

--- car-rental-app/src/components/SmallCarAlbum.jsx
+++ car-rental-app/src/components/SmallCarAlbum.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
 
-import hyundai from "../Assets/images/Hyundai.png";
-import chevrolet from "../Assets/images/chevrolet.png";
-import kia from "../Assets/images/Kia.png";
-import audiA6 from "../Assets/images/Audi_A6.png";
-import audiAll from "../Assets/images/Audi_All.png";
-import bmw from "../Assets/images/BMW.png";
+import HyundaiIoniq6 from "../Assets/images/Hyundai.png";
+import ChevroletCorvetteZ06 from "../Assets/images/chevrolet.png";
+import KiaEV6 from "../Assets/images/Kia.png";
+import AudiA6 from "../Assets/images/Audi_A6.png";
+import AudiA6Allroad from "../Assets/images/Audi_All.png";
+import BMWM4CSL from "../Assets/images/BMW.png";
 
 import samples from "../Assets/sampleCars.json";
 
 function SmallCarAlbum() {
   const [car, setCar] = useState({
     price: samples.HyundaiIoniq6.price,
-    img: hyundai,
+    img: HyundaiIoniq6,
     model: samples.HyundaiIoniq6.model,
     mark: samples.HyundaiIoniq6.mark,
     year: samples.HyundaiIoniq6.year,
@@ -23,87 +23,41 @@ function SmallCarAlbum() {
   });
 
   function handleClick(event) {
-    console.log(event.target.id);
+    let image = null;
     switch (event.target.id) {
-      case "Kia":
-        setCar({
-          price: samples.KiaEV6.price,
-          img: kia,
-          model: samples.KiaEV6.model,
-          mark: samples.KiaEV6.mark,
-          year: samples.KiaEV6.year,
-          doors: samples.KiaEV6.doors,
-          AC: samples.KiaEV6.AC,
-          transmission: samples.KiaEV6.transmission,
-          fuel: samples.KiaEV6.fuel,
-        });
+      case "HyundaiIoniq6":
+        image = HyundaiIoniq6;
         break;
-      case "chevrolet":
-        setCar({
-          price: samples.ChevroletCorvetteZ06.price,
-          img: chevrolet,
-          model: samples.ChevroletCorvetteZ06.model,
-          mark: samples.ChevroletCorvetteZ06.mark,
-          year: samples.ChevroletCorvetteZ06.year,
-          doors: samples.ChevroletCorvetteZ06.doors,
-          AC: samples.ChevroletCorvetteZ06.AC,
-          transmission: samples.ChevroletCorvetteZ06.transmission,
-          fuel: samples.ChevroletCorvetteZ06.fuel,
-        });
+      case "ChevroletCorvetteZ06":
+        image = ChevroletCorvetteZ06;
         break;
-      case "Hyundai":
-        setCar({
-          price: samples.HyundaiIoniq6.price,
-          img: hyundai,
-          model: samples.HyundaiIoniq6.model,
-          mark: samples.HyundaiIoniq6.mark,
-          year: samples.HyundaiIoniq6.year,
-          doors: samples.HyundaiIoniq6.doors,
-          AC: samples.HyundaiIoniq6.AC,
-          transmission: samples.HyundaiIoniq6.transmission,
-          fuel: samples.HyundaiIoniq6.fuel,
-        });
+      case "KiaEV6":
+        image = KiaEV6;
         break;
-      case "Audi_A6":
-        setCar({
-          price: samples.AudiA6.price,
-          img: audiA6,
-          model: samples.AudiA6.model,
-          mark: samples.AudiA6.mark,
-          year: samples.AudiA6.year,
-          doors: samples.AudiA6.doors,
-          AC: samples.AudiA6.AC,
-          transmission: samples.AudiA6.transmission,
-          fuel: samples.AudiA6.fuel,
-        });
+      case "AudiA6":
+        image = AudiA6;
         break;
-      case "Audi_All":
-        setCar({
-          price: samples.AudiA6Allroad.price,
-          img: audiAll,
-          model: samples.AudiA6Allroad.model,
-          mark: samples.AudiA6Allroad.mark,
-          year: samples.AudiA6Allroad.year,
-          doors: samples.AudiA6Allroad.doors,
-          AC: samples.AudiA6Allroad.AC,
-          transmission: samples.AudiA6Allroad.transmission,
-          fuel: samples.AudiA6Allroad.fuel,
-        });
+      case "AudiA6Allroad":
+        image = AudiA6Allroad;
         break;
-      case "BMW":
-        setCar({
-          price: samples.BMWM4CSL.price,
-          img: bmw,
-          model: samples.BMWM4CSL.model,
-          mark: samples.BMWM4CSL.mark,
-          year: samples.BMWM4CSL.year,
-          doors: samples.BMWM4CSL.doors,
-          AC: samples.BMWM4CSL.AC,
-          transmission: samples.BMWM4CSL.transmission,
-          fuel: samples.BMWM4CSL.fuel,
-        });
+      case "BMWM4CSL":
+        image = BMWM4CSL;
         break;
+      default:
+        image = HyundaiIoniq6;
     }
+
+    setCar({
+      price: samples[event.target.id].price,
+      img: image,
+      model: samples[event.target.id].model,
+      mark: samples[event.target.id].mark,
+      year: samples[event.target.id].year,
+      doors: samples[event.target.id].doors,
+      AC: samples[event.target.id].AC,
+      transmission: samples[event.target.id].transmission,
+      fuel: samples[event.target.id].fuel,
+    });
   }
   return (
     <section className="car-album">
@@ -119,14 +73,14 @@ function SmallCarAlbum() {
           </div>
           <div className="album">
             <div onClick={handleClick} className="cars">
-              <div id="Hyundai" className="selected">
+              <div id="HyundaiIoniq6" className="selected">
                 Hyundai Ioniq 6
               </div>
-              <div id="chevrolet">Chevrolet Corvette Z06</div>
-              <div id="Kia">Kia EV6</div>
-              <div id="Audi_A6">Audi A6</div>
-              <div id="Audi_All">Audi A6 Allroad</div>
-              <div id="BMW">BMW M4 CSL</div>
+              <div id="ChevroletCorvetteZ06">Chevrolet Corvette Z06</div>
+              <div id="KiaEV6">Kia EV6</div>
+              <div id="AudiA6">Audi A6</div>
+              <div id="AudiA6Allroad">Audi A6 Allroad</div>
+              <div id="BMWM4CSL">BMW M4 CSL</div>
             </div>
             <div className="car-img">
               <img src={car.img} />
