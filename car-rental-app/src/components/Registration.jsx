c50b5dd88757aae108e5cc683196813a7403c923
unknown <mohamad.ghalebizade@gmail.com>
2024-05-04 21:33:56 -0700

    Add Visibility and VisibilityOff Icons

--- car-rental-app/src/components/Registration.jsx
+++ car-rental-app/src/components/Registration.jsx
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
 import Alert from "@mui/material/Alert";
+import IconButton from "@mui/material/IconButton";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import InputAdornment from "@mui/material/InputAdornment";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import Input from "@mui/material/Input";
+
 import "./registration.css"; // Importing CSS file
 
 function Registration() {
@@ -9,6 +15,7 @@ function Registration() {
     lastName: "",
     email: "",
     password: "",
+    showPassword: false,
   });
 
   const handleChange = (event) => {
@@ -17,6 +24,16 @@ function Registration() {
       ...prevData,
       [name]: value,
     }));
+  };
+
+  function showPassword() {
+    setFormData((prevData) => ({
+      ...prevData,
+      [showPassword]: !formData.showPassword,
+    }));
+  }
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
   };
 
   const handleSubmit = (event) => {
@@ -85,14 +102,32 @@ function Registration() {
           </div>
           <div className="form-group">
             <label htmlFor="password">Password:</label>
-            <input
-              type="password"
+            <Input
+              className="form-control"
+              type={showPassword ? "text" : "password"}
               id="password"
               name="password"
+              minLength={8}
+              maxLength={15}
+              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+              title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
               value={formData.password}
               onChange={handleChange}
               required
-              className="form-control"
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    onClick={showPassword}
+                    onMouseDown={handleMouseDownPassword}
+                  >
+                    {formData.showPassword ? (
+                      <VisibilityIcon />
+                    ) : (
+                      <VisibilityOffIcon />
+                    )}
+                  </IconButton>
+                </InputAdornment>
+              }
             />
           </div>
           <button type="submit" className="btn-register">
