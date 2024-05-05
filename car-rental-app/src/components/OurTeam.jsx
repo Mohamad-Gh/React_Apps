d860aa7a770e09b4c5685361a6bd7dcfa51588e0
unknown <mohamad.ghalebizade@gmail.com>
2024-05-05 12:33:28 -0700

    Adding Carousel component init

--- car-rental-app/src/components/OurTeam.jsx
+++ car-rental-app/src/components/OurTeam.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import BlackDivider from "./BlackDivider";
+import CCarousel from "./CCarousel";
 
 function OurTeam() {
   return (
     <div>
       <Header />
+      <CCarousel />
       <BlackDivider
         text="Book a car by getting in touch with us"
         span="(123) 456-7869"
