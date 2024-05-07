532d778240525f39bf1c60df5338af217a012ca5
unknown <mohamad.ghalebizade@gmail.com>
2024-05-06 22:50:39 -0700

    fix centering grid and improve the css of teamCard.css

--- car-rental-app/src/components/TeamMembersAlbum.jsx
+++ car-rental-app/src/components/TeamMembersAlbum.jsx
@@ -2,30 +2,32 @@ import React from "react";
 import TeamCard from "./TeamCard";
 
 import members from "../Assets/staff.json";
+import "./teamMembersAlbum.css";
 
 function TeamMembersAlbum() {
   return (
-    <div>
-      <div className="title-header">
-        <h3>Meet Our Team</h3>
-        {/* <h2>Our rental fleet</h2> */}
-        <p>
-          At DriveAway Rentals, we pride ourselves on offering a seamless and
-          personalized experience to every customer.
-        </p>
-      </div>
-      <div className="carAlbum">
-        {members.map((member) => {
-          return (
+    <div className="full-container">
+      <div className="containeralbum">
+        <div className="title-header">
+          <h3>Meet Our Team</h3>
+          <p>
+            At DriveAway Rentals, we pride ourselves on offering a seamless and
+            personalized experience to every customer.
+          </p>
+        </div>
+        <div className="team-Album">
+          {members.map((member) => (
             <TeamCard
               key={member.id}
               img={member.img_url}
               name={member.name}
               description={member.description}
             />
-          );
-        })}
+          ))}
+        </div>
+        //{" "}
       </div>
+      //{" "}
     </div>
   );
 }
