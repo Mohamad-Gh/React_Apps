9bcf26bdd772bd1df0a1ef764edfc7feb26395aa
unknown <mohamad.ghalebizade@gmail.com>
2024-05-08 14:38:09 -0700

    Google Map with react googlemap working

--- car-rental-app/src/components/MyGoogleMap.jsx
+++ car-rental-app/src/components/MyGoogleMap.jsx
@@ -1,26 +1,17 @@
 import { useState, useEffect, useCallback, memo } from "react";
-import { GoogleMap, MarkerF, useJsApiLoader } from "@react-google-maps/api";
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
 
 import LocationOnSharpIcon from "@mui/icons-material/LocationOnSharp";
 
 import location from "../Assets/location.json";
-import { useLocation } from "react-router-dom";
-import { colors } from "@mui/material";
 
 const containerStyle = {
   width: "400px",
   height: "400px",
-  colors: "blue",
 };
 
 function MyGoogleMap() {
-  const { isLoaded } = useJsApiLoader({
-    id: "google-map-script",
-    googleMapsApiKey: "",
-  });
-  const [map, setMap] = useState(null);
-
-  const [userLocation, setUserLocation] = useState(null);
+  const [userLocation, setUserLocation] = useState({ lat: "", lng: "" });
 
   useEffect(() => {
     // Get user's location
@@ -31,6 +22,7 @@ function MyGoogleMap() {
             lat: position.coords.latitude,
             lng: position.coords.longitude,
           });
+          console.log(userLocation);
         },
         (error) => {
           console.error("Error getting user location:", error);
@@ -41,33 +33,24 @@ function MyGoogleMap() {
     }
   }, []);
 
-  const onLoad = useCallback(function callback(map) {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds(userLocation);
-    map.fitBounds(bounds);
-
-    setMap(map);
-  }, []);
-
-  const onUnmount = useCallback(function callback() {
-    setMap(null);
-  }, []);
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "",
+  });
 
   return isLoaded ? (
     <GoogleMap
       mapContainerStyle={containerStyle}
       center={{ lat: userLocation.lat, lng: userLocation.lng }}
       zoom={8}
-      onLoad={onLoad}
-      onUnmount={onUnmount}
     >
-      <MarkerF
+      <Marker
         key={0}
         icon={LocationOnSharpIcon}
         position={{ lat: userLocation.lat, lng: userLocation.lng }}
       />
       {location.locations.map((loc, indx) => (
-        <MarkerF
+        <Marker
           key={indx + 1}
           icon={LocationOnSharpIcon}
           position={{ lat: loc.latitude, lng: loc.longitude }}
