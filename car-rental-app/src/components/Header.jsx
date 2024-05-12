299a4da572f8e208b98b4c1ddbad6839c0bb2801
unknown <mohamad.ghalebizade@gmail.com>
2024-05-11 23:41:48 -0700

    add function to add and remove menu in mobile version

--- car-rental-app/src/components/Header.jsx
+++ car-rental-app/src/components/Header.jsx
@@ -1,32 +1,47 @@
 import logo from "../Assets/images/logo.png";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Registration from "./Registration";
 import SignIn from "./SignIn";
 
 function Header() {
   const [popup, setPopup] = useState(false);
   const [sign, setSign] = useState(false);
+  const [bar, setBar] = useState(false);
   function setRigesterPopup() {
     setPopup(!popup);
   }
 
+  // remove the scroll ability when the sign in shows
   if (popup || sign) {
     document.body.classList.add("scroll");
   } else {
     document.body.classList.remove("scroll");
   }
+  document.addEventListener("click", (event) => {
+    if (bar === true && event.target.id !== "menu-bar") {
+      setBar(false);
+    }
+  });
+  document.addEventListener("scroll", (event) => {
+    if (bar === true && event.target.id !== "menu-bar") {
+      setBar(false);
+    }
+  });
+  const showBar = () => {
+    console.log("bar ", bar);
+    setBar(!bar);
+  };
 
   return (
     <header>
       <div class="full-container">
         <div class="container-header">
-          {/* add Link to the Header logo */}
           <Link to="/">
             <img class="company_logo" src={logo} alt="Company Logo" />
           </Link>
           <nav>
-            <ul class="navigator">
+            <ul class={`navigator ${bar && "show-bar"}`}>
               <li>
                 {/* <a href="">Home</a> */}
                 {/* <Route path="/" element={<HomePage}> */}
@@ -62,7 +77,7 @@ function Header() {
               </button>
             </div>
           </div>
-          <div class="mobile-hamb">
+          <div onClick={showBar} class="mobile-hamb">
             <svg
               id="menu-bar"
               xmlns="http://www.w3.org/2000/svg"
