4adf5d5fab17be6fd271bdb4f6710237de7d03be
unknown <mohamad.ghalebizade@gmail.com>
2024-05-14 09:26:33 -0700

    Add Link to Book Ride and Learn More and fix the style in index.css

--- car-rental-app/src/components/Main.jsx
+++ car-rental-app/src/components/Main.jsx
@@ -2,6 +2,7 @@ import carHeroImage from "../Assets/images/Audi-front-page.png";
 import backGroundDesign from "../Assets/images/background_desing.png";
 import googlePlay from "../Assets/images/google_play.png";
 import appStore from "../Assets/images/App_store.png";
+import { Link } from "react-router-dom";
 
 import SmallCarAlbum from "./SmallCarAlbum";
 import BlackDivider from "./BlackDivider";
@@ -28,41 +29,45 @@ function Main() {
                   miles, flexible pick-up options and much more."
                 />
                 <div className="buttons">
-                  <button className="btn btn-book">
-                    Book Ride &nbsp;
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      className="tabler-icon tabler-icon-circle-check"
-                    >
-                      <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
-                      <path d="M9 12l2 2l4 -4"></path>
-                    </svg>
-                  </button>
-                  <button className="btn btn-dark">
-                    Learn More &nbsp;
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      className="tabler-icon tabler-icon-chevron-right"
-                    >
-                      <path d="M9 6l6 6l-6 6"></path>
-                    </svg>
-                  </button>
+                  <Link to="/Vehicle Model">
+                    <button className="btn btn-book">
+                      Book Ride &nbsp;
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        className="tabler-icon tabler-icon-circle-check"
+                      >
+                        <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
+                        <path d="M9 12l2 2l4 -4"></path>
+                      </svg>
+                    </button>
+                  </Link>
+                  <Link to="/About">
+                    <button className="btn btn-dark">
+                      Learn More &nbsp;
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        className="tabler-icon tabler-icon-chevron-right"
+                      >
+                        <path d="M9 6l6 6l-6 6"></path>
+                      </svg>
+                    </button>
+                  </Link>
                 </div>
               </div>
               <img
