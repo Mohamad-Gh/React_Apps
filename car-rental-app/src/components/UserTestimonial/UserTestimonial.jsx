2446c6361bd031e3bfeb4b5271c1c59301412c38
unknown <mohamad.ghalebizade@gmail.com>
2024-05-15 11:54:34 -0700

    add comments to the UserTestimonial.jsx

--- car-rental-app/src/components/UserTestimonial/UserTestimonial.jsx
+++ car-rental-app/src/components/UserTestimonial/UserTestimonial.jsx
@@ -13,6 +13,7 @@ function UserTestimonial(props) {
   });
 
   const handleChange = (event) => {
+    // divide data based on their name
     const { name, value } = event.target;
     setUserInfo((prvs) => {
       return { ...prvs, [name]: value };
@@ -20,45 +21,54 @@ function UserTestimonial(props) {
   };
 
   const handleSubmit = (e) => {
+    // check if the use selected rate and give alert
     if (!userInfo.rate) {
       alert("Plese select the rate");
     } else {
+      // if everything is provided pass info to the parent
       props.info(userInfo);
+      // reset info after submition
       setUserInfo({ name: "", city: "", comment: "", rate: "" });
     }
+    // to preventDefault in all cases
     e.preventDefault();
   };
 
   return (
-    <form className="user-testimonials-box" onSubmit={handleSubmit}>
+    <form
+      className="user-testimonials-box"
+      // monitor change
+      onChange={handleChange}
+      // handlesubmit
+      onSubmit={handleSubmit}
+    >
       <h3>How Was Your Time With Us</h3>
       <div className="user-testimonials-person">
         <label htmlFor="name">Name:</label>
         <input
-          // handlechanges on this input
-          onChange={handleChange}
           id="name"
           placeholder="e.g., Jon Doe"
           name="name"
+          // to set the value based on user input
           value={userInfo.name}
           required
         />
         <label htmlFor="city">City:</label>
         <input
-          onChange={handleChange}
           placeholder={"e.g., Torronto"}
           id="city"
           name="city"
+          // to set the value based on user input
           value={userInfo.city}
           required
         />
         <label htmlFor="comment">Comment:</label>
         <textarea
-          onChange={handleChange}
           rows={4}
           id="comment"
           name="comment"
           placeholder="Write your experience with our fleet"
+          // to set the value based on user input
           value={userInfo.comment}
           required
         >
@@ -67,9 +77,9 @@ function UserTestimonial(props) {
         <div className="userTestimonialRating">
           <h4>Rate Your Experience</h4>
           <Rating
-            onChange={handleChange}
             id="controlled"
             name="rate"
+            // to set the value based on user input
             value={userInfo.rate}
             precision={0.5}
             size="large"
