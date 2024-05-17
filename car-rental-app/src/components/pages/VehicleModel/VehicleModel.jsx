7cf757ef050a24fd047be7925d4eeb6e1d4e218b
unknown <mohamad.ghalebizade@gmail.com>
2024-05-16 23:13:01 -0700

    fix multiple page functionality

--- car-rental-app/src/components/pages/VehicleModel/VehicleModel.jsx
+++ car-rental-app/src/components/pages/VehicleModel/VehicleModel.jsx
@@ -16,20 +16,12 @@ function VehicleModel() {
     luggageCapacity: 2,
     passengerCapacity: 2,
     fuelEfficiency: "none",
-    // for car_model or name
     title: "",
   });
 
-  // .sort((a, b) =>
-  //   change.sortPrice === "ascending"
-  //     ? a.starting_price - b.starting_price
-  //     : b.starting_price - a.starting_price
-  // )
-  // .sort((a, b) =>
-  //   change.fuelEfficiency === "ascending"
-  //     ? a.fuel_efficiency.city - b.fuel_efficiency.city
-  //     : b.fuel_efficiency.city - a.fuel_efficiency.city
-  // )
+  const [currentPage, setCurrentPage] = useState(1);
+  const carsPerPage = 6;
+
   function handleChange(event) {
     const { name, value } = event.target;
     setChange((prvs) => {
@@ -37,67 +29,87 @@ function VehicleModel() {
     });
   }
 
+  const filteredCars = cars.cars
+    .sort((a, b) => {
+      let sortPrice =
+        change.sortPrice === "ascending"
+          ? 1
+          : change.sortPrice === "descending"
+          ? -1
+          : null;
+      let sortFuelEfficiency =
+        change.fuelEfficiency === "ascending"
+          ? 1
+          : change.fuelEfficiency === "descending"
+          ? -1
+          : null;
+
+      return (
+        (sortPrice ? sortPrice * (a.starting_price - b.starting_price) : 0) +
+        (sortFuelEfficiency
+          ? sortFuelEfficiency *
+            (a.fuel_efficiency.city - b.fuel_efficiency.city)
+          : 0)
+      );
+    })
+    .filter(
+      (elmn) =>
+        elmn.luggage_capacity >= change.luggageCapacity &&
+        elmn.passenger_capacity >= change.passengerCapacity &&
+        elmn.car_model.toLowerCase().indexOf(change.title.toLowerCase()) !== -1
+    );
+
+  const indexOfLastCar = currentPage * carsPerPage;
+  const indexOfFirstCar = indexOfLastCar - carsPerPage;
+  const currentCars = filteredCars.slice(indexOfFirstCar, indexOfLastCar);
+
+  const totalPages = Math.ceil(filteredCars.length / carsPerPage);
+
+  const handlePreviousPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+  };
+
   return (
     <div>
       <Header />
       <ControlPanel
-        // user query to search the car
         query={handleChange}
-        // --- these ones are selective based on the options
         price={handleChange}
         luggage={handleChange}
         passenger={handleChange}
         fuel={handleChange}
       />
       <div className="carAlbum">
-        {cars.cars
-          .sort((a, b) => {
-            let sortPrice =
-              change.sortPrice === "ascending"
-                ? 1
-                : change.sortPrice === "descending"
-                ? -1
-                : null;
-            let sortFuelEfficiency =
-              change.fuelEfficiency === "ascending"
-                ? 1
-                : change.fuelEfficiency === "descending"
-                ? -1
-                : null;
-
-            return (
-              sortPrice * (a.starting_price - b.starting_price) +
-              sortFuelEfficiency *
-                (a.fuel_efficiency.city - b.fuel_efficiency.city)
-            );
-          })
-          // filtering based on the toggles and user query
-          .filter(
-            (elmn) =>
-              elmn.luggage_capacity >= change.luggageCapacity &&
-              elmn.passenger_capacity >= change.passengerCapacity &&
-              elmn.car_model
-                .toLowerCase()
-                .indexOf(change.title.toLowerCase()) !== -1
-          )
-          .map((car) => (
-            <CarCard
-              key={car.id}
-              model={car.car_model}
-              city={`${car.fuel_efficiency.city} MPG`}
-              highway={`${car.fuel_efficiency.highway} MPG`}
-              // description={car.car_description}
-              price={`$${car.starting_price} per day`}
-              luggage={car.luggage_capacity}
-              passenger={car.passenger_capacity}
-              transmission={car.features.transmission}
-              AC={car.features.AC}
-              RA={car.features.road_assistant}
-              smoke={car.features.smoke_free}
-              gps={car.features.gps}
-              img={car.image_url}
-            />
-          ))}
+        {currentCars.map((car) => (
+          <CarCard
+            key={car.id}
+            model={car.car_model}
+            city={`${car.fuel_efficiency.city} MPG`}
+            highway={`${car.fuel_efficiency.highway} MPG`}
+            price={`$${car.starting_price} per day`}
+            luggage={car.luggage_capacity}
+            passenger={car.passenger_capacity}
+            transmission={car.features.transmission}
+            AC={car.features.AC}
+            RA={car.features.road_assistant}
+            smoke={car.features.smoke_free}
+            gps={car.features.gps}
+            img={car.image_url}
+          />
+        ))}
+      </div>
+      <div className="pagination">
+        <button onClick={handlePreviousPage} disabled={currentPage === 1}>
+          Previous
+        </button>
+        <span>{`Page ${currentPage} of ${totalPages}`}</span>
+        <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+          Next
+        </button>
       </div>
       <BlackDivider
         text="Book a car by getting in touch with us"
@@ -110,3 +122,116 @@ function VehicleModel() {
 }
 
 export default VehicleModel;
+
+// import React, { useState } from "react";
+
+// import Header from "../../Header/Header";
+// import Footer from "../../Footer/Footer";
+// import BlackDivider from "../../BlackDivider/BlackDivider";
+// import Booking from "../../Booking/Booking";
+// import CarCard from "../../CarCard/CarCard";
+// import ControlPanel from "../../ControlPanel/ControlPanel";
+
+// import "./vehicleModel.css";
+// import cars from "../../../Assets/cars.json";
+
+// function VehicleModel() {
+//   const [change, setChange] = useState({
+//     sortPrice: "none",
+//     luggageCapacity: 2,
+//     passengerCapacity: 2,
+//     fuelEfficiency: "none",
+//     // for car_model or name
+//     title: "",
+//   });
+
+//   // .sort((a, b) =>
+//   //   change.sortPrice === "ascending"
+//   //     ? a.starting_price - b.starting_price
+//   //     : b.starting_price - a.starting_price
+//   // )
+//   // .sort((a, b) =>
+//   //   change.fuelEfficiency === "ascending"
+//   //     ? a.fuel_efficiency.city - b.fuel_efficiency.city
+//   //     : b.fuel_efficiency.city - a.fuel_efficiency.city
+//   // )
+//   function handleChange(event) {
+//     const { name, value } = event.target;
+//     setChange((prvs) => {
+//       return { ...prvs, [name]: value };
+//     });
+//   }
+
+//   return (
+//     <div>
+//       <Header />
+//       <ControlPanel
+//         // user query to search the car
+//         query={handleChange}
+//         // --- these ones are selective based on the options
+//         price={handleChange}
+//         luggage={handleChange}
+//         passenger={handleChange}
+//         fuel={handleChange}
+//       />
+//       <div className="carAlbum">
+//         {cars.cars
+//           .sort((a, b) => {
+//             let sortPrice =
+//               change.sortPrice === "ascending"
+//                 ? 1
+//                 : change.sortPrice === "descending"
+//                 ? -1
+//                 : null;
+//             let sortFuelEfficiency =
+//               change.fuelEfficiency === "ascending"
+//                 ? 1
+//                 : change.fuelEfficiency === "descending"
+//                 ? -1
+//                 : null;
+
+//             return (
+//               sortPrice * (a.starting_price - b.starting_price) +
+//               sortFuelEfficiency *
+//                 (a.fuel_efficiency.city - b.fuel_efficiency.city)
+//             );
+//           })
+//           // filtering based on the toggles and user query
+//           .filter(
+//             (elmn) =>
+//               elmn.luggage_capacity >= change.luggageCapacity &&
+//               elmn.passenger_capacity >= change.passengerCapacity &&
+//               elmn.car_model
+//                 .toLowerCase()
+//                 .indexOf(change.title.toLowerCase()) !== -1
+//           )
+//           .map((car) => (
+//             <CarCard
+//               key={car.id}
+//               model={car.car_model}
+//               city={`${car.fuel_efficiency.city} MPG`}
+//               highway={`${car.fuel_efficiency.highway} MPG`}
+//               // description={car.car_description}
+//               price={`$${car.starting_price} per day`}
+//               luggage={car.luggage_capacity}
+//               passenger={car.passenger_capacity}
+//               transmission={car.features.transmission}
+//               AC={car.features.AC}
+//               RA={car.features.road_assistant}
+//               smoke={car.features.smoke_free}
+//               gps={car.features.gps}
+//               img={car.image_url}
+//             />
+//           ))}
+//       </div>
+//       <BlackDivider
+//         text="Book a car by getting in touch with us"
+//         span="(123) 456-7869"
+//       />
+//       <Booking />
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default VehicleModel;
